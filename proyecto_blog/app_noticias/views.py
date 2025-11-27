650e58d8156dcbf42ba0d549fca7e1c1e4f685e0
Daniel Frias <friasdaniel996@gmail.com>
2025-11-27 19:46:41 -0300

    consultas orm views urls

--- proyecto_blog/app_noticias/views.py
+++ proyecto_blog/app_noticias/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Noticias, Categorias
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+# from .models import Noticias
+# Noticias.objects.all() === SELECT * FROM noticias;
+# Objeto.delete() === DELETE FROM noticias WHERE id=4;
+
+# funciones
+def listar_noticias(request):
+    noticias = Noticias.objects.all()
+
+    ctx = {
+        'noticias': noticias,
+        'titulo_pagina': "Ultimas noticias"
+    }
+
+    return render(request, "app_noticias/listar_noticias.html", ctx)
+
+# clases
